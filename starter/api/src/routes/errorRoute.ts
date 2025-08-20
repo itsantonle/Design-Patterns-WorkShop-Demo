512312acd251887fc29f6b0aa0e97e3ef92e3f06
Wendell Terence Dador <wendellterence.dador-23@cpu.edu.ph>
2025-08-20 12:08:13 +0800

    removed the starter folder

--- starter/api/src/routes/errorRoute.ts
+++
@@ -1,7 +0,0 @@
-import express from 'express'
-import { handleError } from '../controllers/errorController'
-const router = express.Router()
-
-router.route('/').get(handleError)
-
-export default router
--- starter/api/src/routes/todoRoutes.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { createTodo, getTodos } from "../controllers/todoController";
-const router = express.Router();
-
-router.route("/").get(getTodos);
-router.route("/").post(createTodo);
-
-export default router;
